28439db8f054e32f7bdee0bbff8366921bda6f2b
Shkire <danielsmash94@hotmail.com>
2015-01-23 18:59:13 +0100

    Script Giro
    Script de Giro añadido

---
+++ SceneBehaviourScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneBehaviourScript : MonoBehaviour
+{
+
+    private bool giroIzq;
+    private bool giroDer;
+    public float velocidadGiro;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !giroIzq)
+        {
+
+            giroDer = true;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !giroDer)
+        {
+
+            giroIzq = true;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) && giroDer)
+        {
+
+            giroDer = false;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && giroIzq)
+        {
+
+            giroIzq = false;
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        if (giroDer)
+        {
+
+            gameObject.transform.Rotate(-Vector3.forward * velocidadGiro * Time.fixedDeltaTime);
+
+        }
+
+        if (giroIzq)
+        {
+
+            gameObject.transform.Rotate(Vector3.forward * velocidadGiro * Time.fixedDeltaTime);
+
+        }
+
+
+    }
+}
